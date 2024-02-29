7a885b3041ea20cd7c10fd6bc95de7e90aa818b4
namangarg1902 <115886209+namangarg1902@users.noreply.github.com>
2024-02-29 10:19:26 +0530

    Added concept for deleting a file

--- api.js
+++ api.js
@@ -4,19 +4,66 @@ const bodyP = require("body-parser")
 const compiler = require("compilex")
 const options = { stats: true }
 const path = require("path");
+const fs = require('fs');
 
 compiler.init(options)
 app.use(bodyP.json())
 const mainfile = path.join(__dirname, "codemirror-5.65.16");
 app.use("/codemirror-5.65.16", express.static(mainfile))
+
+
+function deleteFilesInFolder(folderPath) {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error('Error reading directory:', err);
+            return;
+        }
+
+        files.forEach(file => {
+            const filePath = path.join(folderPath, file);
+            fs.unlink(filePath, err => {
+                if (err) {
+                    console.error('Error deleting file:', err);
+                    return;
+                }
+                console.log('File deleted successfully:', filePath);
+            });
+        });
+    });
+}
+
+
+
 app.get("/", function (req, res) {
-    compiler.flush(function () {
-        console.log("deleted")
-    })
+    // compiler.flush(function () {
+    //     console.log("deleted")
+    // })
+    const temppath = path.join(__dirname, "temp");
+    deleteFilesInFolder(temppath);
     const publicIPAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const indexPath = path.join(__dirname, "index.html");
     res.sendFile(indexPath , { publicIPAddress });
 })
+
+function deleteFilesInFolder(folderPath) {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error('Error reading directory:', err);
+            return;
+        }
+
+        files.forEach(file => {
+            const filePath = path.join(folderPath, file);
+            fs.unlink(filePath, err => {
+                if (err) {
+                    console.error('Error deleting file:', err);
+                    return;
+                }
+                console.log('File deleted successfully:', filePath);
+            });
+        });
+    });
+}
 app.post("/compile", function (req, res) {
     var code = req.body.code
     var input = req.body.input
